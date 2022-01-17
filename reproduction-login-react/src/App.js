827cfc0c8c3a1af94869593e30bacd3a3bce17d7
Alessandro Veneziano <aveneziano536@gmail.com>
2022-01-17 11:37:31 +0100

    creating default model

--- reproduction-login-react/src/App.js
+++ reproduction-login-react/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+/* screens */
+import Home from './screens/Home/Home';
+import Login from './screens/Login/Login';
+
 function App() {
   return (
     <div className="App">
-
+      <Login />
     </div>
   );
 }
---
+++ reproduction-login-react/src/screens/Login/Login.js
@@ -0,0 +1,18 @@
+import "./Login.css";
+
+import React, { Component } from "react";
+
+
+class Login extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div className="login">
+
+            </div>
+        )
+    }
+}
+export default Login; 
